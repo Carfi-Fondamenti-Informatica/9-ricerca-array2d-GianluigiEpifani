e3f6e777161c6c6f8c176e2c2efcb869b30c31ea
GianluigiEpifani <101205771+GianluigiEpifani@users.noreply.github.com>
2022-05-10 23:27:51 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+
+int main() {
+    int a;
+    char nomi[10][20];
+    char temp[20];
+
+    iniz(nomi);
+    iniz(temp);
+
+    for(int i=0; i<10; i++){
+        cin>>nomi[i]
+    }
+    cin>>temp;
+    a=ricerca(nomi,temp);
+    if(a!=-1)
+        cout<<a;
+    else
+        cout<<"non presente"<<endl;
+
+    return 0;
 }

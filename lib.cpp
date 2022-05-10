100379faeb6d0672338ccbdfa6ded01ab0d53aa6
GianluigiEpifani <101205771+GianluigiEpifani@users.noreply.github.com>
2022-05-10 23:34:00 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,38 @@
+#include "lib.h"
 
+
+void iniz(char x[10][20]){
+    for(int i=0;i<10;i++)
+    {
+        for(int j=0;j<20;j++)
+        {
+            x[i][j]=0;
+        }
+    }
+}
+
+
+void iniz(char x[20]){
+    for(int i=0;i<20;i++)
+    {
+        x[i]=0;
+    }
+}
+
+int ricerca(char nomi[10][20], char temp[20]){
+    int c;
+    for(int i=0;i<10;i++)
+    {
+        c=0;
+        for(int j=0;j<20;j++)
+        {
+            if(nomi[i][j]==temp[j])
+                c++;
+            else
+                break;
+        }
+        if(c==20)
+            return i;
+    }
+    return -1;
+}
